7e97b9e828277b22bf552ab7fdb5373c3c1ca3bb
xiaoxuesheng <23389941@qq.com>
2020-05-19 13:06:59 +0800

    Update Swagger2Config.java

--- Toy/src/main/java/com/hjj/toy/laboratory/config/Swagger2Config.java
+++ Toy/src/main/java/com/hjj/toy/laboratory/config/Swagger2Config.java
@@ -13,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@ConditionalOnProperty(name="swagger.enable",havingValue="true")
 public class Swagger2Config {
     @Bean
     public Docket buildDocket() {
